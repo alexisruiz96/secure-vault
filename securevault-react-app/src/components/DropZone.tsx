ee1d0f3e54d4f7e68af64acff5a396c8df1d7162
alexisruiz96 <alexisruiz2@gmail.com>
2022-08-14 14:34:45 -0400

    Drag n Drop working

--- securevault-react-app/src/components/DropZone.tsx
+++ securevault-react-app/src/components/DropZone.tsx
@@ -1,35 +1,55 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import React, {Dispatch, useCallback, useRef} from 'react'
+import {Accept, useDropzone} from 'react-dropzone'
 import folder from '../assets/folder.png'
 
 
 
-const MyDropzone: React.FC = () => {
-    
+const MyDropzone: React.FC<{setFile:Dispatch<any>}> = ({setFile}) => {
+
+    const acceptedFileTypes: Accept = {
+        'image/png': ['.png']
+    }
+    const inputFile = useRef<HTMLInputElement>(null);
     const onDrop = useCallback((acceptedFiles: any) => {
-        console.log(acceptedFiles)
-    } , [])
-    const {getRootProps, getInputProps} = useDropzone({
-        onDrop,
-        multiple: false,
-    })
+        console.log(acceptedFiles);
+        setFile(acceptedFiles[0]);
+    } , [setFile]);
+
+    const {getRootProps, getInputProps,isDragAccept,isDragReject,} = 
+        useDropzone({
+            onDrop,
+            multiple: false,
+            accept: acceptedFileTypes
+        });
     return (
         <div
-            className='border-2 border-dashed border-yellow-400 rounded-xl mb-5'
+            className={'border-2 border-dashed border-gray-500 rounded-xl mb-5 hover:border-yellow-500 '
+                + (isDragAccept ? ' border-green-500' : '')
+                + (isDragReject ? ' border-red-500' : '')
+            }
+            onClick={() => inputFile.current!.click()}
         >
             <div 
                 {...getRootProps()} 
                 className="w-full h-80 rounded-md cursor-pointer focus:outline-none bg-slate-200"
             >
-                <input {...getInputProps()} />
+                <input {...getInputProps()} ref={inputFile} />
+
                 <div className='justify-center items-center flex flex-col'>
                     <img src={folder} alt="folder" className="w-16 h-16 mt-3" />
-                    <p className="text-center text-lg text-slate-500">
-                        Drag 'n' drop some files here, or click to select files
-                    </p>
-                    <p className='mt-8 text-center text-slate-500'>
-                        Only zips, png & jpg files are allowed
-                    </p>
+                    {isDragReject ? (
+                        <p>Sorry, This app only supports the described types</p>
+                    ) : (
+                        <div>
+                            <p className="text-center text-lg text-slate-500">
+                                Drag 'n' drop some files here, or click to select files
+                            </p>
+                            <p className='mt-8 text-center text-slate-500'>
+                                Only zips, png & jpg files are allowed
+                            </p>
+                        </div>
+
+                    )}
                 </div>
             </div>
 
--- securevault-react-app/src/pages/HomePage.tsx
+++ securevault-react-app/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import {useAuth} from '../api/auth'
 import MyDropzone from '../components/DropZone';
 
@@ -7,7 +7,7 @@ const HomePage:React.FC = () => {
   debugger;
   
   const {user,logout} = useAuth();
-  
+  const [file, setFile] = useState<File | null>(null);
 
   return (
     <div className="App">
@@ -15,7 +15,12 @@ const HomePage:React.FC = () => {
         //TODO add reference to new created page containing user uploaded data
         <div className="welcome bg-slate-500 rounded-md">
           <h2 className='text-center'>Welcome, <span>{user.username}</span></h2>
-          <MyDropzone />
+          <MyDropzone setFile={setFile}/>
+
+          <div className='w-full text-center'>
+            {file?.name}
+          </div>
+          <button className="pt-6">Upload</button>
           <button onClick={logout} className="pt-6">Logout</button>
         </div>
       }
